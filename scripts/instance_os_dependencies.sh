e12d03bc767dd12b41c93cef96402f9916988e0e
masarunakajima <masarun@usc.edu>
2025-01-19 12:25:32 -0800

    update setuptools

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -6,3 +6,4 @@ sudo apt install -y build-essential python3-dev python3-pip
 sudo apt install -y libjpeg-dev zlib1g-dev libpng-dev libfreetype6-dev liblcms2-dev libopenjp2-7-dev libtiff5-dev libwebp-dev tcl-dev tk-dev
 sudo apt install -y nginx
 sudo apt install -y virtualenv
+pip install -U setuptools
