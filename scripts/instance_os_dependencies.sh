cc39325d43741aee469ffe924a3eef36c64b06a3
masarunakajima <masarun@usc.edu>
2025-01-19 11:52:49 -0800

    added more packages

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -3,5 +3,6 @@
 sudo apt update
 sudo apt install -y gcc
 sudo apt install -y build-essential python3-dev python3-pip
+sudo apt install -y libjpeg-dev zlib1g-dev libpng-dev libfreetype6-dev liblcms2-dev libopenjp2-7-dev libtiff5-dev libwebp-dev tcl-dev tk-dev
 sudo apt install -y nginx
 sudo apt install -y virtualenv
