009e8f63caf33069c1fcb9485f05363e283883fa
masarunakajima <masarun@usc.edu>
2025-01-19 11:38:32 -0800

    added gcc

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt update
+sudo apt install -y gcc
 sudo apt install -y build-essential python3-dev python3-pip
 sudo apt install -y nginx
 sudo apt install -y virtualenv
